9109e3a700dc502144a80b244bacf195140ffe01
Maria <sheyko.mari@mail.ru>
2021-03-15 19:54:54 +0300

    feat: task done 50%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let firstPrimer = bracketsConfig[0];
+  let stec = [];
+  brackets = str.split("");
+
+  stec.push(brackets[0]);
+  stec.push(brackets[1]);
+  if ((stec[stec.length - 2] == firstPrimer[0]) && stec[stec.length - 1] == firstPrimer[1]) {
+    stec.pop();
+    stec.pop();
+    return true;
+  } else {
+    return false;
+  }
 }
