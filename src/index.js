06ae1365694a4cf4f90c11a51a65ac4f793dc741
Maria <sheyko.mari@mail.ru>
2022-01-29 14:47:24 +0300

    feat: part of the task done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  let firstPrimer = bracketsConfig[0];
-  let stec = [];
-  brackets = str.split("");
+    console.log(str, bracketsConfig);
+    let openBrackets = [];
+    let bracketsPair = {};
 
-  stec.push(brackets[0]);
-  stec.push(brackets[1]);
-  if ((stec[stec.length - 2] == firstPrimer[0]) && stec[stec.length - 1] == firstPrimer[1]) {
-    stec.pop();
-    stec.pop();
-    return true;
-  } else {
-    return false;
-  }
-}
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        openBrackets.push(bracketsConfig[i][0]);
+        bracketsPair[bracketsConfig[i][1]] = bracketsConfig[i][0];
+    }
+
+    let stec = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let curent = str[i];
+
+        if (openBrackets.includes(curent)) {
+            stec.push(curent);
+        } else if (stec.length === 0) {
+            console.log(false, "1");
+            return false;
+        } else if (bracketsPair[curent] === stec[stec.length - 1]) {
+            stec.pop();
+        } else {
+            console.log(false, "2");
+            return false;
+        }
+    }
+    console.log(stec);
+    if (stec[0] == "|" && stec.length % 2 == 0) {
+        return true;
+    } else {
+        console.log(stec.length === 0, "3");
+
+        return stec.length === 0;
+    }
+};
